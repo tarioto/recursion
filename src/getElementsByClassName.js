6853ecf9fc211acc06439b10ed747a778717b6f5
Tim Arioto <timarioto@gmail.com>
2016-08-28 18:40:40 -0700

    add comments to getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,21 +7,24 @@
 
 var getElementsByClassName = function(className) {
   var resultArray = [];
+  //start of the HTML tree
   var site = document.body;
 
   function pushMatches(className, site) {
-
+    //cheack if current node contains className
     if (site.classList.contains(className)) {
+      //if true push to results array
       resultArray.push(site);
     }
 
     var siteChildren = site.children;
-
+    //iterate over children of current node
     for (var i = 0; i < siteChildren.length; i += 1) {
+      //recursivly call pushMatches function over each child of current node
       pushMatches(className, siteChildren[i]);
     }
   }
-
+  //call recursive function
   pushMatches(className, site);
 
   return resultArray;
